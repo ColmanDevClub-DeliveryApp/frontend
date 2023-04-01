407909fd5836f03bbbc449a7438a601f61f2e5d5
Tal Mekler <tal.mekler11@gmail.com>
2023-04-01 23:32:34 +0300

    remove margin

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import Button from './components/button/index';
 function App() {
   return (
     <div className="App">
+        <Button text={"contained"} type={"contained"} size={"small"} fullWidth/>
+        <Button text={"contained"} type={"contained"} size={"small"}/>
         <Button text={"contained"} type={"contained"} size={"normal"}/>
+        <Button text={"contained"} type={"contained"} size={"big"}/>
+        <Button text={"outlined"} type={"outlined"} size={"small"}/>
+        <Button text={"outlined"} type={"outlined"} size={"normal"}/>
         <Button text={"outlined"} type={"outlined"} size={"big"}/>
         <Button text={"none"} type={"none"} size={"small"}/>
+        <Button text={"none"} type={"none"} size={"normal"}/>
+        <Button text={"none"} type={"none"} size={"big"}/>
     </div>
   );
 }
