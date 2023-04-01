4291755cc8633cce8c61c8d034fd331e1ebab79b
Idan Asayag <idanasayag0@gmail.com>
2023-04-01 22:51:14 +0300

    add the RestaurantInfo components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import './components/button/index'
-import Button from './components/button/index';
+import ResturauntInformation from './components/resturauntInformation/index';
 
 function App() {
+
+  const openingTimes = [
+    { day: ' :א-ה ', hours: ' 9:00-23:00' },
+    { day: ' :שישי', hours: ' 9:00-14:00' },
+    { day: ' :שבת', hours: ' 20:00-23:00' },
+  ];
+
   return (
     <div className="App">
-        <Button text={"contained"} type={"contained"} size={"normal"}/>
-        <Button text={"outlined"} type={"outlined"} size={"big"}/>
-        <Button text={"none"} type={"none"} size={"small"}/>
+      <ResturauntInformation address="האשל 12 ראשון לציון" phone="054-123-1231" openingTimes={openingTimes}/>
     </div>
   );
 }
