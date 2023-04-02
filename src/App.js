fa55151ca19846ed7c0881d64afae646b0030e77
Idan Asayag <idanasayag0@gmail.com>
2023-04-02 11:53:58 +0300

    RestaurantInfo done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import './components/button/index'
-import ResturauntInformation from './components/resturauntInformation/index';
+import PlayGround from './PlayGround';
 
 function App() {
-
-  const openingTimes = [
-    { day: ' :א-ה ', hours: ' 9:00-23:00' },
-    { day: ' :שישי', hours: ' 9:00-14:00' },
-    { day: ' :שבת', hours: ' 20:00-23:00' },
-  ];
-
   return (
     <div className="App">
-      <ResturauntInformation address="האשל 12 ראשון לציון" phone="054-123-1231" openingTimes={openingTimes}/>
+      <PlayGround /> 
     </div>
   );
 }
