8b775993c8adccfd66f09ecff84b974461bbeba0
Tal Mekler <tal.mekler11@gmail.com>
2023-04-02 09:02:56 +0300

    iconButton js and css

---
+++ src/components/iconButton/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css"
+
+/**
+ * 
+ * @param {*} icon 
+ * @param {*} type options: contained / outlined / none
+ * @param {*} size options: small / normal / big
+ */
+
+const IconButton = ({icon, type, size, onclick}) => {
+    return (
+        <div>
+            <button className={`type-${type} size-${size}`} onClick={onclick} >{icon}</button>
+        </div>
+    )
+}
+
+export default IconButton;
