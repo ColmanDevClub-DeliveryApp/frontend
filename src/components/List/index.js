f0fff68dc7b95a46ecd1b4de03d8445cfa8d669c
Idan Asayag <idanasayag0@gmail.com>
2023-04-05 11:44:31 +0300

    add list component

---
+++ src/components/List/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Dish from '../Dish';
+import Style from './styles.module.css';
+
+/**
+ * 
+ * @param {*} items - array of objects
+ * @param {*} listTitle - String
+ * @param {*} subTitle - String 
+ */
+const List = ({items, listTitle, subTitle}) => {
+    return (
+            <div className={Style.list}>
+                {(listTitle || subTitle) && <div className={Style.titles}>
+                {listTitle && <h2 className={Style.list_title}>{listTitle}</h2>}
+                {subTitle && <h3 className={Style.list_sub_title}>{subTitle}</h3>}
+            </div>}
+            <ul className={Style.ul}>
+                {items.map((item, index) =>{
+                    return (
+                    <>
+                        <li className={Style.list_item} key={index}><Dish title={item.title} discription={item.discription} picture={item.picture} price={item.price}/></li>
+                    </>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+};
+
+export default List;
