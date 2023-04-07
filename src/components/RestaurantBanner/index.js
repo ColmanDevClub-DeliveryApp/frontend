0751f6eb12ac6b01d19b853168379e92d9032673
Tal Mekler <tal.mekler11@gmail.com>
2023-04-07 08:44:49 +0300

    cr commands - done

--- src/components/RestaurantBanner/index.js
+++ src/components/RestaurantBanner/index.js
@@ -6,11 +6,14 @@ import Style from "./styles.module.css"
  * @param {String} restTitle the name of the restaurant
  * @param {String} restTitleColor the color of the restaurant's name (light / dark)
  */
-const RestaurantBanner = ({img="https://picsum.photos/200", restTitle="שם המסעדה", restTitleColor="dark"}) => {
+const RestaurantBanner = ({img="https://picsum.photos/1400", restTitle="Restaurant Name", subTitle, restTitleColor="dark"}) => {
 
     return (
         <div className={Style.restaurant_banner} style={{"background": `url(${img}) no-repeat`, "backgroundSize": "100% 100%"}}>
-            <h1 className={`${Style.rest_title} ${restTitleColor === "light" ? Style.light : Style.dark}`}>{restTitle}</h1>
+            <div className={`${Style.titles_container} ${restTitleColor === "light" ? Style.light : Style.dark}`}>
+                <h1 className={`${Style.rest_title}`}>{restTitle}</h1>
+                {subTitle && <h3 className={Style.sub_title}>{subTitle}</h3>}
+            </div>
         </div>
     )
 }
