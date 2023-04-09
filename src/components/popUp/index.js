3562cb1c9d49bf367379b3e2b94b856b4702654b
YuvalOfek1 <yuvalofek97@gmail.com>
2023-04-09 16:51:34 +0300

    added buttons to modal

--- src/components/popUp/index.js
+++ src/components/popUp/index.js
@@ -1,17 +1,29 @@
 import React, {useState, useEffect } from 'react';
 import Styles from './styles.module.css';
 import IconButton from '../iconButton';
+import Button from '../button'
 
-const Popup = ({ isOpen, onClose, children }) => {
+const Popup = ({ isOpen, onClose, isNewUser, setNewUser}) => {
 
-
+    const changeToSignIn = ()=>{
+        if(isNewUser)
+            setNewUser(false);
+    }
+    const changeToSignUp = ()=>{
+        if(!isNewUser)
+            setNewUser(true);
+    }
     return(
         isOpen&&
         <div className={Styles.modal}>
             <div className={Styles.overLay} onClick={onClose}></div>
             <div className={Styles.modalContent}>
-                <IconButton icon='x' size="small" onclick={onClose} type="outlined"/>
-                <h2>התחברות</h2>
+                <IconButton icon='x' onclick={onClose}/>
+                <h2 className={Styles.title}>
+                <Button text="הרשמה" type={`${isNewUser?"contained": "none"}`} onclick={changeToSignUp}/>
+                <Button text="התחברות" type={`${isNewUser?"none": "contained"}`} onclick={changeToSignIn}/>
+
+                </h2>
             </div>
         </div>
 
