c09b41fdb5958afeec3bc1fea3ee67f7e8a711e7
Tal Mekler <tal.mekler11@gmail.com>
2023-04-10 17:58:04 +0300

    add docs

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Style from './styles.module.css';
 
 /**
- * 
+ * @param {*} type | String: the type of the input
+ * @param {*} placeholder | String: the type of the input
+ * @param {*} overrides | Object: using for change the style of the input
  * 
  */
 const Input = ({type="text", placeholder="", overrides={}}) => {
