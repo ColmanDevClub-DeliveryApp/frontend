aed22f2acbe1d7b81f123fb49b6e2d0e172188b4
Tal Mekler <tal.mekler11@gmail.com>
2023-04-26 10:53:49 +0300

    .

--- src/pages/RestaurantPage/index.jsx
+++ src/pages/RestaurantPage/index.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-
+import { useParams } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import RestaurantBanner from "../../components/RestaurantBanner"
+import axios from 'axios'
 // import Navbar from '../../components/Navbar'
 
 function RestaurantPage() {
 
-    const {restaurant_name} = useParams()
+    const {restaurant_name} = useParams();
+    console.log(restaurant_name);
+    axios.get(`http://localhost:8080/restaurants/${restaurant_name}`).then((res)=> {
+        console.log(res.data);
+    }).catch(e=> {
+      console.log(`ERROR: ${e}`);
+    })
+
 
   return (
     <>
         {/* <Link to="/"><Button text="Home" /></Link><br/><br/> */}
         <Navbar />
-        <RestaurantBanner restTitle={restaurant_name}/>
+        <RestaurantBanner restTitle={restaurant_name} restTitleColor="light"/>
     </>
   )
 }
