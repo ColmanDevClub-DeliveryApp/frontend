3db2161f40d75d5979c2b5fabb44c943fa3d3418
Tal Mekler <tal.mekler11@gmail.com>
2023-04-27 17:01:37 +0300

    fix button component + continue work on rest-page

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Style from "./styles.module.css"
+import Style from "./styles.module.css";
 
 /**
- * 
+ *
  * @param {*} text this is the text
  * @param {*} type options: contained / outlined / none
  * @param {*} size options: small / normal / large
@@ -10,12 +10,19 @@ import Style from "./styles.module.css"
  * @param {*} fullWidth add this to the props to make the button 100% width
  */
 
-const Button = ({text="", type="contained", size="normal", onclick, fullWidth=false, overrides={}, children, isDanger}) => {
-    return (
-        <div>
-            <button style={overrides} className={`${Style[type]} ${Style[size]} ${fullWidth ? Style.fullWidth : ""} ${isDanger && Style.danger}`} onClick={onclick}>{children} {text}</button>
-        </div>
-    )
-}
+const Button = ({
+  text = "",
+  type = "contained",
+  size = "normal",
+  onclick,
+  fullWidth = false,
+  overrides = {},
+  children,
+  isDanger,
+}) => {
+  return (
+    <button style={overrides} className={`${Style[type]} ${Style[size]} ${ fullWidth ? Style.fullWidth : ""} ${isDanger && Style.danger}`} onClick={onclick}>{children} {text}</button>
+  );
+};
 
 export default Button;
--- src/pages/RestaurantPage/index.jsx
+++ src/pages/RestaurantPage/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import Navbar from '../../components/Navbar'
 import RestaurantBanner from "../../components/RestaurantBanner"
 import axios from 'axios'
-// import Navbar from '../../components/Navbar'
 
 function RestaurantPage() {
 
@@ -18,9 +16,7 @@ function RestaurantPage() {
 
   return (
     <>
-        {/* <Link to="/"><Button text="Home" /></Link><br/><br/> */}
-        <Navbar />
-        <RestaurantBanner restTitle={restaurant_name} restTitleColor="light"/>
+        <RestaurantBanner restTitle={restaurant_name.toUpperCase()} restTitleColor="light"/>
     </>
   )
 }
