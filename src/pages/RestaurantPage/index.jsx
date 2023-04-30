d962cb9be6821543989cc5128f712e525bf6d205
Tal Mekler <tal.mekler11@gmail.com>
2023-04-30 10:45:45 +0300

    add elements to the page + fix loading bugs

--- src/pages/RestaurantPage/index.jsx
+++ src/pages/RestaurantPage/index.jsx
@@ -1,30 +1,73 @@
 import React, {useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
+import Style from './styles.module.css'
 import RestaurantBanner from "../../components/RestaurantBanner"
-import axios from 'axios'
+// import axios from 'axios'
 import { useState } from 'react';
+import testRest from "../../DB/restaurantDB"
+import RestaurantInfo from "../../components/resturauntInformation"
+import List from "../../components/List"
+import RestaurantSidebar from "../../components/RestaurantSidebar"
+
 
 function RestaurantPage() {
 
-    const {restaurant_name} = useParams();
+    // const {restaurant_name} = useParams();
     const [restaurant, setRestaurant] = useState({})
-    // console.log(restaurant_name);
-    useEffect(() => {
-      axios.get(`http://localhost:8080/restaurants/${restaurant_name.toLowerCase()}`).then((res)=> {
-        console.log(res.data);
-        setRestaurant(res.data)
-      }).catch(e=> {
-        console.log(`ERROR: ${e}`);
-      })
+    const [pageLoaded, setPageLoaded] = useState(false)
+    const [catalogTitles, setCatalogTitles] = useState([])
+
+    // useEffect(() => {
+    //   axios.get(`http://localhost:8080/restaurants/${restaurant_name.toLowerCase()}`).then((res)=> {
+    //     console.log(res.data);
+    //     setRestaurant(res.data)
+    //   }).catch(e=> {
+    //     console.log(`ERROR: ${e}`);
+    //   })
     
-    }, [restaurant_name])
+    // }, [restaurant_name])
     
+    useEffect (()=> {
+      setRestaurant(testRest)
+      setPageLoaded(true)
+    }, [])
+
+    useEffect (()=> {
+      if (pageLoaded)
+        saveCatalogTitles()
+    }, [pageLoaded])
+
+    const saveCatalogTitles = ()=> {
+      const catTitles =[]
+        restaurant.catalog.map((element, index)=> {
+          const x = {"url": `/restaurants/bbb#catalog-${index}`, 'title': element.title}
+          catTitles.push(x)
+        })
+        setCatalogTitles(catTitles)
+    }
     
 
-
-  return (
+  return (pageLoaded&& 
     <>
-        <RestaurantBanner restTitle={restaurant.shownName} subTitle={restaurant.description} restTitleColor="light"/>
+        <RestaurantBanner img={restaurant.image} restTitle={restaurant.shownName} subTitle={restaurant.description}/>
+        <div className={Style.hero}>
+          <input className={Style.search} placeholder='חפש בתפריט'/>
+          <div className={Style.info_wrapper}>
+            <RestaurantInfo />
+          </div>
+          <div className={Style.menu_container}>
+            <div className={Style.menu_wrapper}>
+                {restaurant.catalog.map((catalogItem, index) => {
+                    return (<div className={Style.list_wrapper} id={`catalog-${index}`}>
+                    <List items={catalogItem.dishes} listTitle={catalogItem.title} subTitle={catalogItem.subtitle}/>
+                    </div>)
+                })}
+            </div>
+            <nav className={Style.rest_nav}>
+              <RestaurantSidebar items={catalogTitles} />
+            </nav>
+          </div>
+        </div>
     </>
   )
 }
