a206fb19735464f3f2d077295802330b78ac713f
Idan Asayag <idanasayag0@gmail.com>
2023-05-02 17:25:48 +0300

    continue the homePage

---
+++ src/DB/index.js
@@ -0,0 +1,69 @@
+const category = [
+    {
+        title: "אסייתי",
+        subtitle: "מסעדות אסייתיות",
+        restaurants: [
+            {
+                shownName: "סושי 1",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+            {
+                shownName: "1סושי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+            {
+                shownName: "2סושי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },{
+                shownName: "3סושי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+        ]
+    },{
+        title: "איטלקי",
+        subtitle: "מסעדות איטלקיות",
+        restaurants: [
+            {
+                shownName: "איטלקי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+            {
+                shownName: "1איטלקי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+            {
+                shownName: "2איטקלי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },{
+                shownName: "3אטילקי פלוס",
+                description: "מסעדת סושי",
+                image: "https://picsum.photos/200",
+                deliveryCost: 15,
+                deliveryTime: 30,
+            },
+        ]
+    }
+]
+
+export default category;
