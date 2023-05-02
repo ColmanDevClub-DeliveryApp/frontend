2e0d3f1509ef16442cc33e29b3b2bb81d0b346ae
YuvalOfek1 <yuvalofek97@gmail.com>
2023-05-02 22:15:14 +0300

    added the functionality to get the user and  fetch the last 3 restaurants of the user

--- src/components/userProfile/index.js
+++ src/components/userProfile/index.js
@@ -1,23 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Styles from './styles.module.css';
 import Button from '../button'
 import '@fortawesome/fontawesome-free/css/all.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins } from '@fortawesome/free-solid-svg-icons';
 import RestaurantCard from '../RestaurantCard';
+import axios from 'axios';
 
-const UserProfile = ({fullName="", emailAddress="", phoneNumber="", profilePicture="", coins=0, cardNumber="1234-1234-1234-1234"}) => {
-    const [email, setEmail] = useState(emailAddress);
-    const [phoneNum, setPhoneNum] = useState(phoneNumber);
-    const [profilePic, setProfilePic] = useState(profilePicture);
-    const [specialCoins, setSpecialCoins] = useState(coins);
-    const [paymentDetails, setPaymentDetails] = useState(cardNumber)
+const UserProfile = () => {
 
+  //USE CONNEXT API TO GET USER INFO
+  const user1 = {
+    fullName: "Yuval Ofek",
+    emailAddress: "123@123",
+    phoneNumber: "054-12345678",
+    profilePicture: "https://media.licdn.com/dms/image/C4D03AQGflMHai6AaJA/profile-displayphoto-shrink_800_800/0/1653454376749?e=1688601600&v=beta&t=rdoeeBDdW_aSEUhpXMSgXrjmavsOAi3vun2fJUhrzxo",
+    coins: 20,
+    cardNumber: "1234-5678-1234-5678",
+    lastRestaurants: [
+      {
+        image: "https://picsum.photos/200",
+        name: "restaurant name 3",
+        description: "restaurant description 3",
+        deliveryPrice: 10,
+        deliveryTime: 10,
+      },
+      {
+        image: "https://picsum.photos/200",
+        name: "restaurant name 2",
+        description: "restaurant description 2",
+        deliveryPrice: 20,
+        deliveryTime: 20,
+      },
+      {
+        image: "https://picsum.photos/200",
+        name: "restaurant name 1",
+        description: "restaurant description 1",
+        deliveryPrice: 30,
+        deliveryTime: 30,
+      },
+    ]
+  }
+  
+  const [user, setUser] = useState({user1});
+  const [fullName, setFullName] = useState(user1.fullName);
+  const [email, setEmail] = useState(user1.emailAddress);
+  const [phoneNum, setPhoneNum] = useState(user1.phoneNumber);
+  const [adress, setAdress] = useState(user1.adress);
+  const [profilePic, setProfilePic] = useState(user1.profilePicture);
+  const [specialCoins, setSpecialCoins] = useState(user1.coins);
+  const [paymentDetails, setPaymentDetails] = useState(user1.cardNumber)
+  const [lastRestaurants, setLastRestaurants] = useState(user1.lastRestaurants);
+
+  console.log(fullName)
   const [originalEmail, setOriginalEmail] = useState('');
   const [originalPhoneNumber, setOriginalPhoneNumber] = useState('');
   const [originalProfilePicture, setOriginalProfilePicture] = useState('');
   const [originalPaymentDetails, setOriginalPaymentDetails] = useState('');
-
+  const [originalAdress, setOriginalAdress] = useState('');
   const [editing, setEditing] = useState(false);
 
   useEffect(() => {
@@ -72,7 +112,7 @@ const UserProfile = ({fullName="", emailAddress="", phoneNumber="", profilePictu
   return (
     <div className={Styles.user_profile}>
       <div className={Styles.topBar}>
-        <Button text="מחיקת משתמש" overrides={{"background-color":"red"}} size="small" />
+        <Button text="מחיקת חשבון" overrides={{"background-color":"red"}} size="small" />
         <h1>פרופיל</h1>
       </div>
 
@@ -150,9 +190,9 @@ const UserProfile = ({fullName="", emailAddress="", phoneNumber="", profilePictu
 
         <h1>הזמנות אחרונות</h1>
         <div className={Styles.restaurants}>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
+            <RestaurantCard restaurantItem={lastRestaurants[lastRestaurants.length-1]}/>
+            <RestaurantCard restaurantItem={lastRestaurants[lastRestaurants.length-2]}/>
+            <RestaurantCard restaurantItem={lastRestaurants[lastRestaurants.length-3]}/>
         </div>
         
     </div>
