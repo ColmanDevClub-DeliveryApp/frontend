507ead8deaea55fa2afd5d73f86b13f3102b7f1b
Tal Mekler <tal.mekler11@gmail.com>
2023-05-04 15:53:08 +0300

    .

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Style from './styles.module.css';
+import Navbar from '../../components/Navbar';
+import Carousel from '../../components/Carousel';
+
+const HomePage = () => {
+    return (
+        <>
+        <Navbar />
+        <Carousel />
+        </>
+    )
+}
+
+export default HomePage;
