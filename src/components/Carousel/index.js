6c5f82f1256b63246cc930bd4388eb73fb9957f6
Tal Mekler <tal.mekler11@gmail.com>
2023-05-04 15:44:13 +0300

    fix carousel

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Style from "./styles.module.css";
+import "./carouselStyle.css";
+import RestaurantCard from "../RestaurantCard";
+import { ImArrowRight2, ImArrowLeft2 } from "react-icons/im";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const RestaurantsCarousel = ({
+  restaurants = [],
+  title,
+  subtitle,
+  autoPlay = false,
+  interval = 3000,
+}) => {
+  const handleDragStart = (e) => e.preventDefault();
+  const items = [];
+
+  restaurants.forEach((item) => {
+    items.push(
+      <div className={Style.card} onDragStart={handleDragStart}>
+        <RestaurantCard restaurantItem={item} />
+      </div>
+    );
+  });
+
+  return (
+    <div className={Style.carouselComponent}>
+      {(title || subtitle) && (
+        <div className={Style.titles}>
+          {title && <h1 className={Style.title}>{title}</h1>}
+          {subtitle && <h3 className={Style.subtitle}>{subtitle}</h3>}
+        </div>
+      )}
+
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={{
+          0: {
+            items: 1,
+          },
+          1024: {
+            items: 3,
+            itemsFit: "contain",
+          },
+        }}
+        infinite
+        autoPlay={autoPlay}
+        autoPlayInterval={interval}
+        disableDotsControls
+        autoHeight={false}
+        renderNextButton={()=> {return <div className="alice-carousel__next-btn-wrapper"><ImArrowRight2/></div>}}
+        renderPrevButton={()=> {return <div className="alice-carousel__prev-btn-wrapper"><ImArrowLeft2/></div>}}
+      />
+    </div>
+  );
+};
+
+export default RestaurantsCarousel;
