9181fe975c0ea7bba5b2af8d5b6caba9a1f501de
YuvalOfek1 <yuvalofek97@gmail.com>
2023-05-14 22:51:18 +0300

    added token to local storage and post the user details

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Style from './styles.module.css';
-import Button from "../Button";
-
+import Button from "../Button/index.js";
+import axios from 'axios';
 
 const SignUpForm = () => {
+
+  const [formDetails, setFormDetails] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    phoneNum: ''
+  });
+
+
+  const handleSubmit = (e)=>{
+    // const config = {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': localStorage.getItem('token')
+    //   }
+    // }
+    e.preventDefault();
+    axios.post('http://localhost:8080/signup/',{...formDetails, firstName: formDetails.fullName.split(' ')[0], lastName: formDetails.fullName.split(' ')[1]}).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  const handleChange = (e) => {
+    setFormDetails({...formDetails, [e.target.name]: e.target.value});
+  }
+
+
   return (
     <div className={Style.SignUp}>
         <form className={Style.form}>
             <div className={Style.input_container}>
-                <input className={`${Style.input}`} type={"text"} placeholder={"שם מלא"}></input>
-                <input className={`${Style.input}`} type={"text"} placeholder={"אימייל"}></input>
-                <input className={`${Style.input}`} type={"password"} placeholder={"סיסמא"}></input>
-                <input className={`${Style.input}`} type={"text"} placeholder={"טלפון"}></input>
+                <input className={Style["input"]} type={"text"} placeholder="שם מלא" name='fullName' onChange={handleChange}></input>
+                <input className={Style["input"]} type={"email"} placeholder="אימייל" name='email' onChange={handleChange}></input>
+                <input className={Style["input"]} type={"password"} placeholder="סיסמא" name='password' onChange={handleChange}></input>
+                <input className={Style["input"]} type={"tel"} placeholder="טלפון" name='phoneNum' onChange={handleChange}></input>
             </div>
-            <Button text="הרשמה" fullWidth/>
+            <Button text="הרשמה" fullWidth onclick={handleSubmit}/>
         </form>
     </div>
   );
--- src/components/signInForm/index.js
+++ src/components/signInForm/index.js
@@ -2,20 +2,41 @@ import React, {useState} from "react";
 import Button from "../Button";
 import Styles from './styles.module.css'
 import {FaGoogle} from 'react-icons/fa'
-
+import axios from "axios";
 const SignInForm = () =>{
+    const [formDetails, setFormDetails] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setFormDetails({...formDetails, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        axios.post('http://localhost:8080/signin/',{
+            email: formDetails.email,
+            password: formDetails.password
+        }).then((res)=>{
+            console.log(res.data);
+            res.data ? localStorage.setItem('token', res.data) : console.log("no token");
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
 
     return(
         <div className={Styles.signIn}>
         <form className={Styles.form}>
             <div className={Styles.inputsContainer}>
-                <input className={Styles.input} type="email" placeholder="אימייל"></input>
-                <input className={`${Styles.input} ${Styles.lastInput}`} type="password" placeholder="סיסמא"></input>
+                <input className={Styles.input} type="email" placeholder="אימייל" name="email" onChange={handleChange}></input>
+                <input className={`${Styles.input} ${Styles.lastInput}`} type="password" placeholder="סיסמא" name="password" onChange={handleChange}></input>
                 <div className={Styles.forgotPassword}>
                     <Button text="שכחתי את הסיסמא" size="small" type="none" fullWidth overrides={{"padding":"0"}}/>
                 </div>
             </div>
-            <Button text="התחבר" fullWidth/>
+            <Button text="התחבר" fullWidth onclick={handleSubmit}/>
         </form>
         <Button text="Continue with Google" fullWidth type="outlined">
         <FaGoogle className={Styles.google_icon}/>
