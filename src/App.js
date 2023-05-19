c2e56721f99473e5613c6772b89e8405cf806c11
Tal Mekler <tal.mekler11@gmail.com>
2023-05-19 15:01:05 +0300

    Background->App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
-import PlayGround from "./PlayGround";
-// import Navbar from './components/Navbar';
-// import HomePage from './pages/HomePage';
+import { Route, Routes } from "react-router-dom";
+import RestaurantPage from "./pages/RestaurantPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+
+import { RestaurantProvider } from "./components/RestaurantProvider";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
     <div className="App">
-      <PlayGround />
-      {/* <Navbar />
-      <HomePage /> */}
+      <Navbar />
+      <div style={{ flex: "1" }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route
+            path="/restaurants/:restaurant_name"
+            element={
+              <RestaurantProvider>
+                <RestaurantPage />
+              </RestaurantProvider>
+            }
+          />
+          <Route
+            path="/restaurants/:restaurant_name/order"
+            element={
+              <RestaurantProvider>
+                <OrderPage />
+              </RestaurantProvider>
+            }
+          />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
